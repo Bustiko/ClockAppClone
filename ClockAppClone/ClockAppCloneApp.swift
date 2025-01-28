c5812c56ce567aa96e91644b2c0c2146ce3a1f5d
Buse Karabıyık <karabiyikbuse04@gmail.com>
2025-01-28 21:17:13 +0300

    Initial Commit

---
+++ ClockAppClone/ClockAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppCloneApp.swift
+//  ClockAppClone
+//
+//  Created by Buse Karabıyık on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
